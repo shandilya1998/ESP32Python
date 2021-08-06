460cc14cc067e52bcd2f9eac4ccb0d063d681a42
Shreyas Shandilya <shandilya.shreyas@gmail.com>
2021-08-07 00:36:36 +0530

    commit control and stream app template

--- src/opencv.py
+++ src/opencv.py
@@ -9,13 +9,38 @@ def test_stream(url):
         bytes += stream.read(1024)
         a = bytes.find(b'\xff\xd8')
         b = bytes.find(b'\xff\xd9')
-        if a != -1 and b != -1:
-            jpg = bytes[a:b+2]
-            bytes = bytes[b+2:]
-            i = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
-            cv2.imshow('i', i)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
+        try:
+            if a != -1 and b != -1:
+                jpg = bytes[a:b+2]
+                bytes = bytes[b+2:]
+                i = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
+                cv2.imshow('i', i)
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+
+        except cv2.error:
+            pass
+
+def sample_images(url, assets_dir, num_images = 20):
+    stream = urlopen(url)
+    bytes = b'' 
+    count = 0
+    while True:
+        bytes += stream.read(1024)
+        a = bytes.find(b'\xff\xd8')
+        b = bytes.find(b'\xff\xd9')
+        try:
+            if a != -1 and b != -1: 
+                jpg = bytes[a:b+2]
+                bytes = bytes[b+2:]
+                i = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
+                cv2.imwrite(os.path.join('assets_dir', str(count)+'.png'), i)
+                count += 1
+        except cv2.error:
+            pass
+        if count > num_images:
+            break
+
 
 if __name__ == '__main__':
     url = 'http://192.168.29.183:81/stream'
