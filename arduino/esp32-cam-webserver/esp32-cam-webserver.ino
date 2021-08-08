63c6411989018b95edb035fd3a34c0a2d8b6fbf0
Shreyas Shandilya <shandilya.shreyas@gmail.com>
2021-08-08 21:58:49 +0530

    commit functional mobile stream on home WiFi network as well

--- arduino/esp32-cam-webserver/esp32-cam-webserver.ino
+++ arduino/esp32-cam-webserver/esp32-cam-webserver.ino
@@ -95,7 +95,7 @@ extern void serialDump();
 #endif
 
 #if !defined(WIFI_WATCHDOG)
-    #define WIFI_WATCHDOG 5000
+    #define WIFI_WATCHDOG 10000
 #endif
 
 // Number of known networks in stationList[]
